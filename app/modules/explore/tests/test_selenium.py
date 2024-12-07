7f9d90b2c6ee819df82ccd04dff417d328dc1d92
aloporsan <aloporsan@alum.us.es>
2024-12-07 19:16:32 +0100

    test(advanced-filtering): test de selenium funcionando.
    Se han corregido los tests de Selenium de tal manera que funcionen y se pasen

--- app/modules/explore/tests/test_selenium.py
+++ app/modules/explore/tests/test_selenium.py
@@ -32,95 +32,108 @@ class TestExplorePage(unittest.TestCase):
         self.assertGreater(len(results.find_elements(By.CLASS_NAME, 'card')), 0, "No se encontraron resultados")
         print("Funcionalidad de búsqueda: OK")
 
-    # def test_filter_by_publication_type(self):
-    #     publication_type_filter = self.driver.find_element(By.ID, 'publication_type')
-    #     self.driver.execute_script("arguments[0].scrollIntoView();", publication_type_filter)
-    #     self.wait.until(EC.element_to_be_clickable((By.ID, 'publication_type')))
-    #     publication_type_filter.click()
-    #     option = self.wait.until(EC.element_to_be_clickable((By.XPATH, ".//option[text()='Journal']")))
-    #     option.click()
-    #     time.sleep(2)
+    def test_filter_by_publication_type(self):
+        dropdown = self.driver.find_element(By.ID, "publication_type")
+        dropdown.find_element(By.XPATH, "//option[. = 'Data Management Plan']").click()
 
-    #     # Verifica que los resultados están filtrados por tipo de publicación
-    #     results = self.driver.find_element(By.ID, 'results')
-    #     for card in results.find_elements(By.CLASS_NAME, 'card'):
-    #         self.assertIn("Journal", card.text)
-    #     print("Filtro por tipo de publicación: OK")
+        # Espera a que el elemento "Sample dataset 2" sea clickable
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Sample dataset 2"))
+        )
 
-    # def test_filter_by_author(self):
-    #     author_filter = self.driver.find_element(By.ID, 'authors')
-    #     self.driver.execute_script("arguments[0].scrollIntoView();", author_filter)
-    #     self.wait.until(EC.element_to_be_clickable((By.ID, 'authors')))
-    #     author_filter.click()
-    #     option = self.wait.until(EC.element_to_be_clickable((By.XPATH, ".//option[text()='John Doe']")))
-    #     option.click()
-    #     time.sleep(2)
+        # Desplazar la vista hasta el elemento si es necesario
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
 
-    #     # Verifica que los resultados están filtrados por autor
-    #     results = self.driver.find_element(By.ID, 'results')
-    #     for card in results.find_elements(By.CLASS_NAME, 'card'):
-    #         self.assertIn("John Doe", card.text)
-    #     print("Filtro por autor: OK")
+        # Hacer clic en el elemento
+        element.click()
 
-    # def test_filter_by_files(self):
-    #     files_filter = self.driver.find_element(By.ID, 'files')
-    #     self.driver.execute_script("arguments[0].scrollIntoView();", files_filter)
-    #     self.wait.until(EC.element_to_be_clickable((By.ID, 'files')))
-    #     files_filter.click()
-    #     option = self.wait.until(EC.element_to_be_clickable((By.XPATH, ".//option[text()='1file']")))
-    #     option.click()
-    #     time.sleep(2)
+        print("Filtro por tipo de publicación: OK")
+        
+    def test_filter_by_author(self):
+        dropdown = self.driver.find_element(By.ID, "authors")
+        dropdown.find_element(By.XPATH, "//option[. = 'Author 2']").click()
 
-    #     # Verifica que los resultados están filtrados por número de archivos
-    #     results = self.driver.find_element(By.ID, 'results')
-    #     for card in results.find_elements(By.CLASS_NAME, 'card'):
-    #         self.assertIn("1 file", card.text)
-    #     print("Filtro por número de archivos: OK")
+        # Espera a que el elemento "Sample dataset 2" sea clickable
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Sample dataset 2"))
+        )
 
-    # def test_filter_by_size(self):
-    #     size_filter = self.driver.find_element(By.ID, 'size')
-    #     self.driver.execute_script("arguments[0].scrollIntoView();", size_filter)
-    #     self.wait.until(EC.element_to_be_clickable((By.ID, 'size')))
-    #     size_filter.click()
-    #     option = self.wait.until(EC.element_to_be_clickable((By.XPATH, ".//option[text()='lessThan1KB']")))
-    #     option.click()
-    #     time.sleep(2)
+        # Desplazar la vista hasta el elemento si es necesario
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
 
-    #     # Verifica que los resultados están filtrados por tamaño
-    #     results = self.driver.find_element(By.ID, 'results')
-    #     for card in results.find_elements(By.CLASS_NAME, 'card'):
-    #         self.assertIn("less than 1 KB", card.text)
-    #     print("Filtro por tamaño: OK")
+        # Hacer clic en el elemento
+        element.click()
 
-    # def test_filter_by_title(self):
-    #     title_filter = self.driver.find_element(By.ID, 'title')
-    #     self.driver.execute_script("arguments[0].scrollIntoView();", title_filter)
-    #     self.wait.until(EC.element_to_be_clickable((By.ID, 'title')))
-    #     title_filter.click()
-    #     option = self.wait.until(EC.element_to_be_clickable((By.XPATH, ".//option[text()='Example Title']")))
-    #     option.click()
-    #     time.sleep(2)
+        print("Filtro por autor: OK")
 
-    #     # Verifica que los resultados están filtrados por título
-    #     results = self.driver.find_element(By.ID, 'results')
-    #     for card in results.find_elements(By.CLASS_NAME, 'card'):
-    #         self.assertIn("Example Title", card.text)
-    #     print("Filtro por título: OK")
+    def test_filter_by_files(self):
+        dropdown = self.driver.find_element(By.ID, "files")
+        dropdown.find_element(By.XPATH, "//option[. = '1 File']").click()
 
-    # def test_filter_by_tag(self):
-    #     tag_filter = self.driver.find_element(By.ID, 'tag')
-    #     self.driver.execute_script("arguments[0].scrollIntoView();", tag_filter)
-    #     self.wait.until(EC.element_to_be_clickable((By.ID, 'tag')))
-    #     tag_filter.click()
-    #     option = self.wait.until(EC.element_to_be_clickable((By.XPATH, ".//option[text()='Example Tag']")))
-    #     option.click()
-    #     time.sleep(2)
+        # Espera a que el elemento "Sample dataset 2" sea clickable
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Sample dataset 2"))
+        )
 
-    #     # Verifica que los resultados están filtrados por etiqueta
-    #     results = self.driver.find_element(By.ID, 'results')
-    #     for card in results.find_elements(By.CLASS_NAME, 'card'):
-    #         self.assertIn("Example Tag", card.text)
-    #     print("Filtro por etiqueta: OK")
+        # Desplazar la vista hasta el elemento si es necesario
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+
+        # Hacer clic en el elemento
+        element.click()
+
+        print("Filtro por archivos: OK")
+
+    def test_filter_by_size(self):
+        dropdown = self.driver.find_element(By.ID, "size")
+        dropdown.find_element(By.XPATH, "//option[. = 'Less than 1KB']").click()
+
+        # Espera a que el elemento "Sample dataset 2" sea clickable
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Sample dataset 2"))
+        )
+
+        # Desplazar la vista hasta el elemento si es necesario
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+
+        # Hacer clic en el elemento
+        element.click()
+
+        print("Filtro por tamaño: OK")
+
+    def test_filter_by_title(self):
+        dropdown = self.driver.find_element(By.ID, "title")
+        dropdown.find_element(By.XPATH, "//option[. = 'Sample dataset 2']").click()
+
+        # Espera a que el elemento "Sample dataset 2" sea clickable
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Sample dataset 2"))
+        )
+
+        # Desplazar la vista hasta el elemento si es necesario
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+
+        # Hacer clic en el elemento
+        element.click()
+
+        print("Filtro por titulo: OK")
+
+    def test_filter_by_tag(self):
+
+        dropdown = self.driver.find_element(By.ID, "tag")
+        dropdown.find_element(By.XPATH, "//option[. = 'Any']").click()
+
+        # Espera a que el elemento "Sample dataset 2" sea clickable
+        element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Sample dataset 2"))
+        )
+
+        # Desplazar la vista hasta el elemento si es necesario
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+
+        # Hacer clic en el elemento
+        element.click()
+
+        print("Filtro por tag: OK")
 
     def test_clear_filters(self):
         clear_button = self.driver.find_element(By.ID, 'clear-filters')
